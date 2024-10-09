3df1ced895da7420d4b4e527ac12ae45f5667e58
Joanadayse <daysejoana3@gmail.com>
2024-10-09 09:53:11 -0300

    busca cep

---
+++ src/Endereco.java
@@ -0,0 +1,2 @@
+public record Endereco(String cep , String lagradouro, String complemento, String bairro , String localidade, String uf) {
+}
---
+++ src/Principal.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+ConsultaCep consultaCep = new ConsultaCep();
+
+System.out.println("Digite o CEP: ");
+var cep = leitura.nextLine();
+
+
+ try{
+     Endereco novoEndereco= consultaCep.buscaEndereco(cep);
+     System.out.println(novoEndereco);
+
+     GeradorDeArquivo gerador=new GeradorDeArquivo();
+     gerador.salvaJson(novoEndereco);
+ } catch (RuntimeException e) {
+     System.out.println(e.getMessage());
+     System.out.println("Finalizou a aplicação!");
+ } catch (IOException e) {
+     throw new RuntimeException(e);
+ }
+
+
+    }
+}
